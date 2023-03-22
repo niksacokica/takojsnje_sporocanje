96ebd7570361c003fbaa4533c8720b8b4379402e
niksacokica <65570607+niksacokica@users.noreply.github.com>
2023-03-22 13:55:24 +0100

    part 2 done

---
+++ Contact.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Windows;
+
+namespace takojsnje_sporocanje{
+    public class Contact : INotifyPropertyChanged{
+        private string name = "";
+        private string email = "";
+        private string phone = "";
+        private string avatar = "";
+        private List<Tuple<string, string>> convo = new();
+        private DateTime lastMessage = DateTime.Now;
+
+        public Contact(string name, string email, string phone, string avatar, List<Tuple<string, string>> convo, DateTime lastMessage)
+        {
+            Name = name;
+            Email = email;
+            PhoneNumber = phone;
+            Avatar = avatar;
+            Conversation = convo;
+            LastMessage = lastMessage;
+        }
+
+        public string Name{
+            get { return name; }
+            set{
+                if(value.Length >= 3)
+                    name = value;
+                else{
+                    name = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+                    MessageBox.Show("Ime mora vsebovati vsaj 3 znake!", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        public string Email{
+            get { return email; }
+            set{
+                if(new EmailAddressAttribute().IsValid(value)){
+                    email = value;
+                    NotifyPropertyChanged("Email");
+                }else
+                    MessageBox.Show("Napačen e-poštni naslov!", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public string PhoneNumber{
+            get { return phone; }
+            set{
+                phone = value;
+                NotifyPropertyChanged("PhoneNumber");
+            }
+        }
+
+        public string Avatar{
+            get { return avatar; }
+            set{
+                if(File.Exists(value)){
+                    avatar = value;
+                    NotifyPropertyChanged("Avatar");
+                }else
+                    MessageBox.Show("Datoteka ne obstaja!", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public List<Tuple<string, string>> Conversation{
+            get { return convo; }
+            set{
+                convo = value;
+                NotifyPropertyChanged("Conversation");
+            }
+        }
+
+        public DateTime LastMessage{
+            get { return lastMessage; }
+            set{
+                lastMessage = value;
+                NotifyPropertyChanged("LastMessage");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public override string ToString(){
+            return Name;
+        }
+
+        private void NotifyPropertyChanged(string Name){
+            if(PropertyChanged != null){
+                PropertyChanged(this, new PropertyChangedEventArgs(Name));
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,32 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace takojsnje_sporocanje{
     public partial class MainWindow : Window{
+        ObservableCollection<Contact> contacts = new();
+
         public MainWindow(){
             InitializeComponent();
+
+            contacts.Add(new("Mama", "mama@email.com", "123456789", "../../../images/user_avatar.png", new(), DateTime.Now));
+            contacts.Add(new("Ata", "ata@email.com", "987654321", "../../../images/user_avatar.png", new(), DateTime.Now));
+            contacts.Add(new("Sestra", "sestra@email.com", "1593574862", "../../../images/user_avatar.png", new(), DateTime.Now));
+
+            stiki.ItemsSource = contacts;
         }
 
         private void ExitApp(object sender, RoutedEventArgs e){
             Application.Current.Shutdown();
+        }
+
+        private void stiki_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e){
+            var item = (ListView) sender;
+            if(item != null && item.SelectedItem != null){
+
+                MessageBox.Show(item.SelectedItem.ToString());
+            }
         }
     }
 }
